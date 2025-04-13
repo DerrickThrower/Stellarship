e63d8b3c55fbc3706ec43e69c4ca52c7c2c0e24a
Harrison Tran <harrit4@uci.edu>
2025-04-13 11:49:26 -0700

    basic mongo implementation

--- backend/starter.py
+++ backend/starter.py
@@ -1,32 +1,52 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-
+import pprint
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from pymongo import ReplaceOne
 
-uri = "mongodb+srv://harrit4:<db_password>@cluster0.dtapwpl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# replace <db_password> with your actual password
+uri = "mongodb+srv://harrit4:mAE10694yQBjLIfY@cluster0.dtapwpl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-# Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+db = client.scholarships
 
+# scrapes the scholarship website for info cuhhhh
 driver = webdriver.Chrome()
 driver.get("https://www.scholarships.com/financial-aid/college-scholarships/scholarships-by-type/first-in-family-scholarships")
-
 
 soup = BeautifulSoup(driver.page_source, "html.parser")
 
 titles = soup.find_all("h2", class_="margin-zero font-size-lg-2")
 amounts = soup.find_all("li",  style="margin-left:20px;")
+deadline_elements = soup.find_all("li", style="margin-left:20px;")
+deadline = []
+for i in deadline_elements:
+    deadline.append(i.find_next('li'))
 
-for i, ii in zip(titles, amounts):
-    print('Title: ' + i.text)
-    print('Amount: ' + ii.text.split('Amount')[1])
+# initializes a list object to put the data in
+
+documents = []
+"""
+for i, ii,iii in zip(titles, amounts,deadline):
+    print(i.text + " " + ii.text.split('Amount')[1] + " " + iii.text.split('Deadline')[1])"""
+# iterates through the titles and amounts and appends them to the list
+for i, ii, iii, iv in zip(titles, amounts, deadline, range(len(titles))):
+    documents.append({
+        '_id': str(iv),
+        'title': i.text,
+        'amount': ii.text.split('Amount')[1],
+        'deadline': iii.text.split('Deadline')[1]
+    })
+
+operations = [
+    ReplaceOne({"_id": doc["_id"]}, doc, upsert=True)
+    for doc in documents
+]
+scholarships = db.titlesandamounts
+scholarships.bulk_write(operations)
+for i in scholarships.find():
+    pprint.pprint(i)
 
 driver.quit()
