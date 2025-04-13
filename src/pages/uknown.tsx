f9c88137de37f69365b7b2d30c10a41e28581d2f
Derrick Thrower <75774890+Jacksopop@users.noreply.github.com>
2025-04-13 11:16:42 -0700

    lol

--- src/pages/uknown.tsx
+++ src/pages/uknown.tsx
@@ -2,6 +2,15 @@ import React from 'react'
 import { Chatbot } from '../components/chatbot'
 
 const Projects = () => {
+  
+  
+  
+  
+  
+  //loop through the mongo db and for each scholarship make a card and display it on the page
+  //make the cards clickable and when clicked it should redirect to the scholarship page
+  
+  
   return (
     <div>
       <h1>Projects Page</h1>
